94ad63f26b03a50f04e5ff787de2fabb8c7da13d
rayGreycloud <rayGreycloud@gmail.com>
2016-07-26 11:52:56 -0700

    Add express server

---
+++ index.js
@@ -0,0 +1,17 @@
+// Main starting point of App
+// import dependencies
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/* '}));
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on port:', port);
