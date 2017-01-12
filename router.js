c85670501971903197725218c77c900e6ffc62a9
rayGreycloud <rayGreycloud@gmail.com>
2017-01-12 07:06:33 -0800

    Wire up passport middleware with test route

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// passport middleware
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+  // test route for auth handling
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hi: 'authorized!'});
+  });
   app.post('/signup', Authentication.signup);
 }
