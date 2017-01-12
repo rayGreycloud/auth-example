8534829d6c2507edaa521a16e84b716c539643b4
rayGreycloud <rayGreycloud@gmail.com>
2017-01-12 07:26:46 -0800

    Add localStrategy and scaffold local strategy creation

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,21 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local Strategy
+// Specify email property for username
+const localOptions = { usernameField: 'email' };
+// Pull out username and password
+const localLogin = new LocalStrategy({ localOptions }, function(email, password, done) {
+  // Check username and password
+
+  // If verified, call done with user
+
+  // If not, call done with false
+
+  
+});
 
 // Set options for JWT Strategy
 const jwtOptions = {
