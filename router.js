18184d8660f54860ad7126912f22decd30c64df2
rayGreycloud <rayGreycloud@gmail.com>
2017-01-12 08:37:51 -0800

    Add signin middleware to router

--- router.js
+++ router.js
@@ -4,11 +4,14 @@ const passport = require('passport');
 
 // passport middleware
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   // test route for auth handling
   app.get('/', requireAuth, function(req, res) {
     res.send({ hi: 'authorized!'});
   });
+
+  app.get('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 }
